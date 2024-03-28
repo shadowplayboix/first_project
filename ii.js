a3f4cce469e6b5cfd0b9f0444aff5c4eada05724
shadowplayboix <165239222+shadowplayboix@users.noreply.github.com>
2024-03-28 10:19:04 +0600

    first commit

---
+++ ii.js
@@ -0,0 +1,21 @@
+// let = users[ '1','2', '3'];
+// for ( i = 0 ; i < users .length; i++) ; {
+//     console.log(users)
+// }
+
+let users = ['tilek', 'altena,', 'raysv'];
+
+// for(let i = 0; i<users.length; i++){
+//     console.log(users[i]);
+// }
+
+// for (let user of users){
+//     if(user==='altena')continue;
+//     document.write(user);
+// }
+let i = 0;
+
+while (i<users.length){
+    document.write(users[1]);
+    i++;
+}
